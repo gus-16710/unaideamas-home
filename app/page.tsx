b8e8c3c9453179e7d3585d4a6c27e3707ab4a717
Gustavo <gus16710@gmail.com>
2025-10-28 14:45:27 -0600

    Maintance mode

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-col items-center justify-center w-full max-w-3xl p-16 bg-white dark:bg-black">
         <Image
           src="/img/logo-light.png"
           alt="unaidea+ logo"
@@ -11,7 +11,7 @@ export default function Home() {
           height={20}
           priority
         />
-        <h1 className="mt-16 text-sm font-bold text-zinc-800 dark:text-zinc-200 sm:text-5xl">
+        <h1 className="mt-8 text-sm text-zinc-800 dark:text-zinc-200">
           🚧 En construcción 🚧
         </h1>
       </main>
