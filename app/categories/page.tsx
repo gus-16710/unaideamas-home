d08881c4f49b2c7fa89ea32ce269e37ad5cb2000
Gustavo <gus16710@gmail.com>
2025-10-31 14:07:25 -0600

    home/categories page

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -448,7 +448,7 @@ export default function Categories() {
         </motion.div>
 
         {/* Categories Grid */}
-        <div className="w-full mb-16">
+        <div className="w-full mb-10">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full">
             {categories.map((category, index) => {
               const productCount = getProductsCountByCategory(category.url);
